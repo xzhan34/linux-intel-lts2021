5dc3bfb77c10d2acaa6fffd629ec0263d1147ea6
Michał Winiarski <michal.winiarski@intel.com>
2020-03-19 16:03:48 +0100

    INTEL_DII: drm/i915/vf: Update list of early runtime regs for PVC
    Compared to XEHPSDV, PVC VF driver needs one additional register,
    GEN12_GT_COMPUTE_DSS_ENABLE_EXT that was introduced on PVC.
    
    v2 (Piotr):
    - add GEN10_MIRROR_FUSE3 register to PVC runtime regs table (VLK-17797)
    - remove unused GEN11_GT_SLICE_ENABLE register from table
    - replace register GEN11_EU_DISABLE by XEHPSDV_EU_ENABLE
    - start using identifier GEN12_GT_COMPUTE_DSS_ENABLE_EXT instead of register
    address
    
    v3: add GEN11_HUC_KERNEL_LOAD_INFO (michal)
    
    Bspec: 53818
    
    Signed-off-by: Michał Winiarski <michal.winiarski@intel.com>
    Cc: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Cc: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Cc: Tomasz Lis <tomasz.lis@intel.com>
    Signed-off-by: Piotr Piórkowski <piotr.piorkowski@intel.com>

--- drivers/gpu/drm/i915/gt/iov/intel_iov_query.c
+++ drivers/gpu/drm/i915/gt/iov/intel_iov_query.c
@@ -521,12 +521,27 @@ static const i915_reg_t dg2_early_regs[] = {
 	GEN11_HUC_KERNEL_LOAD_INFO,	/* _MMIO(0xC1DC) */
 };
 
+static const i915_reg_t pvc_early_regs[] = {
+	RPM_CONFIG0,			/* _MMIO(0x0D00) */
+	GEN10_MIRROR_FUSE3,		/* _MMIO(0x9118) */
+	XEHP_EU_ENABLE,			/* _MMIO(0x9134) */
+	GEN12_GT_GEOMETRY_DSS_ENABLE,	/* _MMIO(0x913C) */
+	GEN11_GT_VEBOX_VDBOX_DISABLE,	/* _MMIO(0x9140) */
+	GEN12_GT_COMPUTE_DSS_ENABLE,	/* _MMIO(0x9144) */
+	XEHPC_GT_COMPUTE_DSS_ENABLE_EXT,/* _MMIO(0x9148) */
+	CTC_MODE,			/* _MMIO(0xA26C) */
+	GEN11_HUC_KERNEL_LOAD_INFO,	/* _MMIO(0xC1DC) */
+};
+
 static const i915_reg_t *get_early_regs(struct drm_i915_private *i915,
 					unsigned int *size)
 {
 	const i915_reg_t *regs;
 
-	if (IS_DG2(i915)) {
+	if (IS_PONTEVECCHIO(i915)) {
+		regs = pvc_early_regs;
+		*size = ARRAY_SIZE(pvc_early_regs);
+	} else if (IS_DG2(i915)) {
 		regs = dg2_early_regs;
 		*size = ARRAY_SIZE(dg2_early_regs);
 	} else if (IS_XEHPSDV(i915)) {
