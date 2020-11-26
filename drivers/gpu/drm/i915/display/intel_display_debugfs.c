be9d03e48549b579847b3bd914b6494be378ee1f
Bhanuprakash Modem <bhanuprakash.modem@intel.com>
2020-11-26 20:06:28 +0530

    INTEL_DII: drm/i915/display/debug: Expose Dither status via debugfs
    It's useful to know the dithering state & pipe bpc for IGT testing.
    This patch will expose the dithering state for the crtc via a debugfs
    file "dither".
    
    Example usage: cat /sys/kernel/debug/dri/0/crtc-0/dither
    
    v2:
    * Rebase (Bhanu)
    v3:
    * Read dither status from register instead of crtc state variable (Bhanu)
    * Add support to read dithering after CC1 block (Bhanu)
    v4:
    * Minor clean-ups (Uma)
    v5:
    * Read registers inside of their respective powerwells (Chris)
    * Create a new patch for dither after CC1, it can be easy to upstream
      the legacy one (Bhanu)
    v6:
    * Read from the state variable instead of reading registers (Matt/Uma)
    * Expose pipe bpc (Bhanu)
    
    Cc: Uma Shankar <uma.shankar@intel.com>
    Cc: Nischal Varide <nischal.varide@intel.com>
    Cc: Matt Roper <matthew.d.roper@intel.com>
    Signed-off-by: Bhanuprakash Modem <bhanuprakash.modem@intel.com>
    Reviewed-by: Uma Shankar <uma.shankar@intel.com>

--- drivers/gpu/drm/i915/display/intel_display_debugfs.c
+++ drivers/gpu/drm/i915/display/intel_display_debugfs.c
@@ -1966,10 +1966,40 @@ static const struct {
 	{"i915_suppress_wakeup_hpd", &i915_suppress_wakeup_hpd_fops},
 };
 
+static int dither_state_show(struct seq_file *m, void *data)
+{
+	struct intel_crtc *crtc = to_intel_crtc(m->private);
+	struct drm_i915_private *dev_priv = to_i915(crtc->base.dev);
+	struct intel_crtc_state *crtc_state;
+	int ret;
+
+	if (!HAS_DISPLAY(dev_priv))
+		return -ENODEV;
+
+	ret = drm_modeset_lock_single_interruptible(&crtc->base.mutex);
+	if (ret)
+		return ret;
+
+	crtc_state = to_intel_crtc_state(crtc->base.state);
+	seq_printf(m, "bpc: %u\n", crtc_state->pipe_bpp / 3);
+	seq_printf(m, "Dither: %u\n", (crtc_state->dither) ? 1 : 0);
+
+	drm_modeset_unlock(&crtc->base.mutex);
+
+	return 0;
+}
+DEFINE_SHOW_ATTRIBUTE(dither_state);
+
 void intel_display_debugfs_register(struct drm_i915_private *i915)
 {
 	struct drm_minor *minor = i915->drm.primary;
+	struct drm_device *dev = &i915->drm;
+	struct drm_crtc *crtc;
 	int i;
+
+	drm_for_each_crtc(crtc, dev)
+		debugfs_create_file("dither", 0444, crtc->debugfs_entry, crtc,
+				    &dither_state_fops);
 
 	for (i = 0; i < ARRAY_SIZE(intel_display_debugfs_files); i++) {
 		debugfs_create_file(intel_display_debugfs_files[i].name,
