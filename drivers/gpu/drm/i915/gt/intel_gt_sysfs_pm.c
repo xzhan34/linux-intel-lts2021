961e46ef1acf240104ad3fd7fa9f1cccaafa1eb8
Dale B Stimson <dale.b.stimson@intel.com>
2021-01-22 17:05:15 -0800

    INTEL_DII: drm/i915/sysfs: gt/sysfs_gt_pm.c checkpatch and cleanup
    Fix things about which checkpatch complained except as noted below.
    
    Ignore benign checkpatch CamelCase complaint:
    "CHECK: Avoid CamelCase: <RPn_freq_mhz_show>"
    This module provides certain sysfs attributes whose names contain
    "RPn".  By default, checkpatch considers these names to be a use of
    CamelCase and issues a warning.  These names have been previously
    exposed via sysfs and it is considered undesirable to change them.
    For purposes of this cleanup, these checkpatch warnings were suppressed
    using checkpatch option --ignore=CAMELCASE.
    
    Ignore benign checkpatch MACRO_ARG_REUSE complaint:
    "CHECK: Macro argument reuse '__mode' - possible side-effects?"
    "CHECK: Macro argument reuse '__show' - possible side-effects?"
    "CHECK: Macro argument reuse '__store' - possible side-effects?"
    Overridden with --ignore=MACRO_ARG_REUSE
    
    Clean up and reorganize some code.
    
    Function declaration continuation indentation alignment.
    
    Add comments.
    
    v2: Rebase after SLPC backport (Vinay)
    
    Signed-off-by: Dale B Stimson <dale.b.stimson@intel.com>
    Reviewed-by: Jon Ewins <jon.ewins@intel.com>

--- drivers/gpu/drm/i915/gt/intel_gt_sysfs_pm.c
+++ drivers/gpu/drm/i915/gt/intel_gt_sysfs_pm.c
@@ -113,6 +113,8 @@ static ssize_t media_rc6_residency_ms_show(struct device *dev,
 	return scnprintf(buff, PAGE_SIZE, "%u\n", rc6_residency);
 }
 
+/* sysfs dual-location rc6 files under directories <dev>/power/ and <dev>/gt/gt<i>/ */
+
 static DEVICE_ATTR_RW(rc6_enable);
 static DEVICE_ATTR_RO(rc6_residency_ms);
 static DEVICE_ATTR_RO(rc6p_residency_ms);
@@ -154,12 +156,29 @@ static const struct attribute_group media_rc6_attr_group[] = {
 static int __intel_gt_sysfs_create_group(struct kobject *kobj,
 					 const struct attribute_group *grp)
 {
+	/* is_object_gt() returns 0 if parent device or 1 if gt/gt<i>. */
 	int i = is_object_gt(kobj);
 
+	/*
+	 * For gt/gt<i>, sysfs_create_group() from grp[1] - group name = "".
+	 * For <parent>, sysfs_merge_group()  from grp[0] - group name = "power"
+	 * which must already exist.
+	 */
 	return i ? sysfs_create_group(kobj, &grp[i]) :
 		   sysfs_merge_group(kobj, &grp[i]);
 }
 
+/*
+ * intel_sysfs_rc6_init()
+ * @gt: The gt being processed.
+ * @kobj: The kobj in sysfs to which created files will be attached.
+ *
+ * Called unconditionally from intel_gt_sysfs_pm_init:
+ * - Once with kobj specifying directory of parent_device (and gt specifying gt0).
+ *   Places files under <dev>/power
+ * - Once per gt, with kobj specifying directory gt/gt<i>
+ *   Places files under <dev>/gt/gt<i>.
+ */
 static void intel_sysfs_rc6_init(struct intel_gt *gt, struct kobject *kobj)
 {
 	int ret;
@@ -194,8 +213,18 @@ static void intel_sysfs_rc6_init(struct intel_gt *gt, struct kobject *kobj)
 }
 #endif /* CONFIG_PM */
 
-static ssize_t act_freq_mhz_show(struct device *dev,
+static ssize_t vlv_rpe_freq_mhz_show(struct device *dev,
 				     struct device_attribute *attr, char *buff)
+{
+	struct intel_gt *gt = intel_gt_sysfs_get_drvdata(dev, attr->attr.name);
+	struct intel_rps *rps = &gt->rps;
+
+	return scnprintf(buff, PAGE_SIZE, "%d\n",
+			intel_gpu_freq(rps, rps->efficient_freq));
+}
+
+static ssize_t act_freq_mhz_show(struct device *dev,
+				 struct device_attribute *attr, char *buff)
 {
 	struct intel_gt *gt = intel_gt_sysfs_get_drvdata(dev, attr->attr.name);
 
@@ -240,16 +269,6 @@ static ssize_t boost_freq_mhz_store(struct device *dev,
 	ret = intel_rps_set_boost_frequency(rps, val);
 
 	return ret ?: count;
-}
-
-static ssize_t vlv_rpe_freq_mhz_show(struct device *dev,
-				     struct device_attribute *attr, char *buff)
-{
-	struct intel_gt *gt = intel_gt_sysfs_get_drvdata(dev, attr->attr.name);
-	struct intel_rps *rps = &gt->rps;
-
-	return scnprintf(buff, PAGE_SIZE, "%d\n",
-			intel_gpu_freq(rps, rps->efficient_freq));
 }
 
 static ssize_t max_freq_mhz_show(struct device *dev,
@@ -309,31 +328,82 @@ static ssize_t min_freq_mhz_store(struct device *dev,
 	return ret ?: count;
 }
 
-#define INTEL_GT_RPS_SYSFS_ATTR(_name, _mode, _show, _store) \
-	struct device_attribute dev_attr_gt_##_name = __ATTR(gt_##_name, _mode, _show, _store); \
-	struct device_attribute dev_attr_rps_##_name = __ATTR(rps_##_name, _mode, _show, _store)
+static ssize_t RP0_freq_mhz_show(struct device *dev,
+				 struct device_attribute *attr,
+				 char *buff)
+{
+	struct intel_gt *gt = intel_gt_sysfs_get_drvdata(dev, attr->attr.name);
+	struct intel_rps *rps = &gt->rps;
+	struct intel_guc_slpc *slpc = &gt->uc.guc.slpc;
+	u32 val;
 
+	if (intel_uc_uses_guc_slpc(&gt->uc))
+		val = slpc->rp0_freq;
+	else
+		val = intel_gpu_freq(rps, rps->rp0_freq);
+
+	return scnprintf(buff, PAGE_SIZE, "%d\n", val);
+}
+
+static ssize_t RP1_freq_mhz_show(struct device *dev,
+				 struct device_attribute *attr,
+				 char *buff)
+{
+	struct intel_gt *gt = intel_gt_sysfs_get_drvdata(dev, attr->attr.name);
+	struct intel_rps *rps = &gt->rps;
+	struct intel_guc_slpc *slpc = &gt->uc.guc.slpc;
+	u32 val;
+
+	if (intel_uc_uses_guc_slpc(&gt->uc))
+		val = slpc->rp1_freq;
+	else
+		val = intel_gpu_freq(rps, rps->rp1_freq);
+
+	return scnprintf(buff, PAGE_SIZE, "%d\n", val);
+}
+
+static ssize_t RPn_freq_mhz_show(struct device *dev,
+				 struct device_attribute *attr,
+				 char *buff)
+{
+	struct intel_gt *gt = intel_gt_sysfs_get_drvdata(dev, attr->attr.name);
+	struct intel_rps *rps = &gt->rps;
+	struct intel_guc_slpc *slpc = &gt->uc.guc.slpc;
+	u32 val;
+
+	if (intel_uc_uses_guc_slpc(&gt->uc))
+		val = slpc->min_freq;
+	else
+		val = intel_gpu_freq(rps, rps->min_freq);
+
+	return scnprintf(buff, PAGE_SIZE, "%d\n", val);
+}
+
+/* sysfs dual-location files <dev>/vlv_rpe_freq_mhz and <dev>/gt/gt0/vlv_rpe_freq_mhz */
+static DEVICE_ATTR_RO(vlv_rpe_freq_mhz);
+
+/* sysfs dual-location files <dev>/gt_* and <dev>/gt/gt<i>/rps_* */
+
+#define INTEL_GT_RPS_SYSFS_ATTR(_name, __mode, __show, __store) \
+	static struct device_attribute dev_attr_gt_##_name =    \
+		__ATTR(gt_##_name, __mode, __show, __store);    \
+	static struct device_attribute dev_attr_rps_##_name =   \
+		__ATTR(rps_##_name, __mode, __show, __store)
+
+/* Note: rps_ and gt_ share common show and store functions. */
 #define INTEL_GT_RPS_SYSFS_ATTR_RO(_name)				\
 		INTEL_GT_RPS_SYSFS_ATTR(_name, 0444, _name##_show, NULL)
 #define INTEL_GT_RPS_SYSFS_ATTR_RW(_name)				\
 		INTEL_GT_RPS_SYSFS_ATTR(_name, 0644, _name##_show, _name##_store)
 
-static INTEL_GT_RPS_SYSFS_ATTR_RO(act_freq_mhz);
-static INTEL_GT_RPS_SYSFS_ATTR_RO(cur_freq_mhz);
-static INTEL_GT_RPS_SYSFS_ATTR_RW(boost_freq_mhz);
-static INTEL_GT_RPS_SYSFS_ATTR_RW(max_freq_mhz);
-static INTEL_GT_RPS_SYSFS_ATTR_RW(min_freq_mhz);
-
-static DEVICE_ATTR_RO(vlv_rpe_freq_mhz);
-
-static ssize_t rps_rp_mhz_show(struct device *dev,
-			       struct device_attribute *attr,
-			       char *buff);
-
-static INTEL_GT_RPS_SYSFS_ATTR(RP0_freq_mhz, 0444, rps_rp_mhz_show, NULL);
-static INTEL_GT_RPS_SYSFS_ATTR(RP1_freq_mhz, 0444, rps_rp_mhz_show, NULL);
-static INTEL_GT_RPS_SYSFS_ATTR(RPn_freq_mhz, 0444, rps_rp_mhz_show, NULL);
-
+INTEL_GT_RPS_SYSFS_ATTR_RO(act_freq_mhz);
+INTEL_GT_RPS_SYSFS_ATTR_RO(cur_freq_mhz);
+INTEL_GT_RPS_SYSFS_ATTR_RW(boost_freq_mhz);
+INTEL_GT_RPS_SYSFS_ATTR_RW(max_freq_mhz);
+INTEL_GT_RPS_SYSFS_ATTR_RW(min_freq_mhz);
+INTEL_GT_RPS_SYSFS_ATTR_RO(RP0_freq_mhz);
+INTEL_GT_RPS_SYSFS_ATTR_RO(RP1_freq_mhz);
+INTEL_GT_RPS_SYSFS_ATTR_RO(RPn_freq_mhz);
 
 #define GEN6_ATTR(s) { \
 		&dev_attr_##s##_act_freq_mhz.attr, \
@@ -347,42 +417,15 @@ static INTEL_GT_RPS_SYSFS_ATTR(RPn_freq_mhz, 0444, rps_rp_mhz_show, NULL);
 		NULL, \
 	}
 
-#define GEN6_RPS_ATTR GEN6_ATTR(rps)
-#define GEN6_GT_ATTR  GEN6_ATTR(gt)
+/* sysfs files <dev>/gt_* */
+static const struct attribute * const gen6_rps_attrs[] = GEN6_ATTR(rps);
 
-/* For now we have a static number of RP states */
-static ssize_t rps_rp_mhz_show(struct device *dev,
-			       struct device_attribute *attr,
-			       char *buff)
-{
-	struct intel_gt *gt = intel_gt_sysfs_get_drvdata(dev, attr->attr.name);
-	struct intel_rps *rps = &gt->rps;
-	u32 val;
-
-	if (attr == &dev_attr_gt_RP0_freq_mhz ||
-	    attr == &dev_attr_rps_RP0_freq_mhz) {
-		val = intel_rps_get_rp0_frequency(rps);
-	} else if (attr == &dev_attr_gt_RP1_freq_mhz ||
-	           attr == &dev_attr_rps_RP1_freq_mhz) {
-		val = intel_rps_get_rp1_frequency(rps);
-	} else if (attr == &dev_attr_gt_RPn_freq_mhz ||
-	           attr == &dev_attr_rps_RPn_freq_mhz) {
-		val = intel_rps_get_rpn_frequency(rps);
-	} else {
-		GEM_WARN_ON(1);
-		return -ENODEV;
-	}
-
-	return scnprintf(buff, PAGE_SIZE, "%d\n", val);
-
-}
-
-static const struct attribute * const gen6_rps_attrs[] = GEN6_RPS_ATTR;
-static const struct attribute * const gen6_gt_attrs[]  = GEN6_GT_ATTR;
+/* sysfs files <dev>/gt/gt<i>/rps_* */
+static const struct attribute * const gen6_gt_attrs[]  = GEN6_ATTR(gt);
 
 static ssize_t rapl_PL1_freq_mhz_show(struct device *dev,
-					struct device_attribute *attr,
-					char *buff)
+				      struct device_attribute *attr,
+				      char *buff)
 {
 	struct intel_gt *gt = intel_gt_sysfs_get_drvdata(dev, attr->attr.name);
 	u32 rapl_pl1 = intel_rps_read_rapl_pl1_frequency(&gt->rps);
@@ -391,8 +434,8 @@ static ssize_t rapl_PL1_freq_mhz_show(struct device *dev,
 }
 
 static ssize_t punit_req_freq_mhz_show(struct device *dev,
-					struct device_attribute *attr,
-					char *buff)
+				       struct device_attribute *attr,
+				       char *buff)
 {
 	struct intel_gt *gt = intel_gt_sysfs_get_drvdata(dev, attr->attr.name);
 	u32 swreq = intel_rps_get_requested_frequency(&gt->rps);
@@ -400,9 +443,9 @@ static ssize_t punit_req_freq_mhz_show(struct device *dev,
 	return scnprintf(buff, PAGE_SIZE, "%d\n", swreq);
 }
 
-static ssize_t throttle_reason_show(struct device *dev,
-					struct device_attribute *attr,
-					char *buff)
+static ssize_t throttle_reason_status_show(struct device *dev,
+					   struct device_attribute *attr,
+					   char *buff)
 {
 	struct intel_gt *gt = intel_gt_sysfs_get_drvdata(dev, attr->attr.name);
 	bool status = !!intel_rps_read_throttle_reason_status(&gt->rps);
@@ -441,8 +484,8 @@ static ssize_t throttle_reason_pl4_show(struct device *dev,
 }
 
 static ssize_t throttle_reason_thermal_show(struct device *dev,
-					struct device_attribute *attr,
-					char *buff)
+					    struct device_attribute *attr,
+					    char *buff)
 {
 	struct intel_gt *gt = intel_gt_sysfs_get_drvdata(dev, attr->attr.name);
 	bool thermal = !!intel_rps_read_throttle_reason_thermal(&gt->rps);
@@ -451,8 +494,8 @@ static ssize_t throttle_reason_thermal_show(struct device *dev,
 }
 
 static ssize_t throttle_reason_prochot_show(struct device *dev,
-					struct device_attribute *attr,
-					char *buff)
+					    struct device_attribute *attr,
+					    char *buff)
 {
 	struct intel_gt *gt = intel_gt_sysfs_get_drvdata(dev, attr->attr.name);
 	bool prochot = !!intel_rps_read_throttle_reason_prochot(&gt->rps);
@@ -461,8 +504,8 @@ static ssize_t throttle_reason_prochot_show(struct device *dev,
 }
 
 static ssize_t throttle_reason_ratl_show(struct device *dev,
-					struct device_attribute *attr,
-					char *buff)
+					 struct device_attribute *attr,
+					 char *buff)
 {
 	struct intel_gt *gt = intel_gt_sysfs_get_drvdata(dev, attr->attr.name);
 	bool ratl = !!intel_rps_read_throttle_reason_ratl(&gt->rps);
@@ -471,8 +514,8 @@ static ssize_t throttle_reason_ratl_show(struct device *dev,
 }
 
 static ssize_t throttle_reason_vr_thermalert_show(struct device *dev,
-					struct device_attribute *attr,
-					char *buff)
+						  struct device_attribute *attr,
+						  char *buff)
 {
 	struct intel_gt *gt = intel_gt_sysfs_get_drvdata(dev, attr->attr.name);
 	bool thermalert = !!intel_rps_read_throttle_reason_vr_thermalert(&gt->rps);
@@ -481,8 +524,8 @@ static ssize_t throttle_reason_vr_thermalert_show(struct device *dev,
 }
 
 static ssize_t throttle_reason_vr_tdc_show(struct device *dev,
-					struct device_attribute *attr,
-					char *buff)
+					   struct device_attribute *attr,
+					   char *buff)
 {
 	struct intel_gt *gt = intel_gt_sysfs_get_drvdata(dev, attr->attr.name);
 	bool tdc = !!intel_rps_read_throttle_reason_vr_tdc(&gt->rps);
@@ -490,17 +533,21 @@ static ssize_t throttle_reason_vr_tdc_show(struct device *dev,
 	return scnprintf(buff, PAGE_SIZE, "%u\n", tdc);
 }
 
-static DEVICE_ATTR(rapl_PL1_freq_mhz, 0444, rapl_PL1_freq_mhz_show, NULL);
-static DEVICE_ATTR(punit_req_freq_mhz, 0444, punit_req_freq_mhz_show, NULL);
-static DEVICE_ATTR(throttle_reason_status, 0444, throttle_reason_show, NULL);
-static DEVICE_ATTR(throttle_reason_pl1, 0444, throttle_reason_pl1_show, NULL);
-static DEVICE_ATTR(throttle_reason_pl2, 0444, throttle_reason_pl2_show, NULL);
-static DEVICE_ATTR(throttle_reason_pl4, 0444, throttle_reason_pl4_show, NULL);
-static DEVICE_ATTR(throttle_reason_thermal, 0444, throttle_reason_thermal_show, NULL);
-static DEVICE_ATTR(throttle_reason_prochot, 0444, throttle_reason_prochot_show, NULL);
-static DEVICE_ATTR(throttle_reason_ratl, 0444, throttle_reason_ratl_show, NULL);
-static DEVICE_ATTR(throttle_reason_vr_thermalert, 0444, throttle_reason_vr_thermalert_show, NULL);
-static DEVICE_ATTR(throttle_reason_vr_tdc, 0444, throttle_reason_vr_tdc_show, NULL);
+/* dgfx sysfs files under directory <dev>/gt/gt<i>/ */
+static DEVICE_ATTR_RO(rapl_PL1_freq_mhz);
+
+/* gen12+ sysfs files under directory <dev>/gt/gt<i>/ */
+
+static DEVICE_ATTR_RO(punit_req_freq_mhz);
+static DEVICE_ATTR_RO(throttle_reason_status);
+static DEVICE_ATTR_RO(throttle_reason_pl1);
+static DEVICE_ATTR_RO(throttle_reason_pl2);
+static DEVICE_ATTR_RO(throttle_reason_pl4);
+static DEVICE_ATTR_RO(throttle_reason_thermal);
+static DEVICE_ATTR_RO(throttle_reason_prochot);
+static DEVICE_ATTR_RO(throttle_reason_ratl);
+static DEVICE_ATTR_RO(throttle_reason_vr_thermalert);
+static DEVICE_ATTR_RO(throttle_reason_vr_tdc);
 
 static const struct attribute *freq_attrs[] = {
 	&dev_attr_punit_req_freq_mhz.attr,
@@ -516,14 +563,34 @@ static const struct attribute *freq_attrs[] = {
 	NULL
 };
 
-static int intel_sysfs_rps_init(struct intel_gt *gt, struct kobject *kobj,
-				const struct attribute * const *attrs)
+static int intel_sysfs_rps_init_gt(struct intel_gt *gt, struct kobject *kobj)
 {
 	int ret;
 
-	if (GRAPHICS_VER(gt->i915) < 6)
-		return 0;
+	if (GRAPHICS_VER(gt->i915) >= 12) {
+		ret = sysfs_create_files(kobj, freq_attrs);
+		if (ret)
+			return ret;
+	}
 
+	if (IS_DGFX(gt->i915)) {
+		ret = sysfs_create_file(kobj, &dev_attr_rapl_PL1_freq_mhz.attr);
+		if (ret)
+			return ret;
+	}
+
+	return 0;
+}
+
+static int intel_sysfs_rps_init(struct intel_gt *gt, struct kobject *kobj)
+{
+	const struct attribute * const *attrs;
+	int ret;
+
+	if (is_object_gt(kobj))
+		attrs = gen6_rps_attrs;
+	else
+		attrs = gen6_gt_attrs;
 	ret = sysfs_create_files(kobj, attrs);
 	if (ret)
 		return ret;
@@ -534,28 +601,39 @@ static int intel_sysfs_rps_init(struct intel_gt *gt, struct kobject *kobj,
 			return ret;
 	}
 
-	if ((GRAPHICS_VER(gt->i915) >= 12) && (is_object_gt(kobj))) {
-		ret = sysfs_create_files(kobj, freq_attrs);
+	if (is_object_gt(kobj)) {
+		/* attributes for only directory gt/gt<i> */
+		ret = intel_sysfs_rps_init_gt(gt, kobj);
 		if (ret)
 			return ret;
 	}
 
-	if ((IS_DGFX(gt->i915)) && (is_object_gt(kobj)))
-		ret = sysfs_create_file(kobj, &dev_attr_rapl_PL1_freq_mhz.attr);
-
-	return ret;
+	return 0;
 }
 
+/*
+ * intel_gt_sysfs_pm_init()
+ * @gt: The gt being processed.
+ * @kobj: The kobj in sysfs to which created files will be attached.
+ *
+ * Called twice:
+ * - Once with kobj == the device parent directory and gt == gt0.
+ *   Populates those things whose parent directory is kobj.
+ * - Once per gt, with kobj == that gt's kobject = gt/gt<i>
+ *   Populates those things whose parent directory is gt/gt<i>.
+ */
 void intel_gt_sysfs_pm_init(struct intel_gt *gt, struct kobject *kobj)
 {
 	int ret;
 
 	intel_sysfs_rc6_init(gt, kobj);
 
-	ret = is_object_gt(kobj) ?
-	      intel_sysfs_rps_init(gt, kobj, gen6_rps_attrs) :
-	      intel_sysfs_rps_init(gt, kobj, gen6_gt_attrs);
-	if (ret)
-		drm_err(&gt->i915->drm,
-			"failed to create gt%u RPS sysfs files", gt->info.id);
+	if (GRAPHICS_VER(gt->i915) >= 6) {
+		ret = intel_sysfs_rps_init(gt, kobj);
+		if (ret) {
+			drm_err(&gt->i915->drm,
+				"failed to create gt%u RPS sysfs files",
+				gt->info.id);
+		}
+	}
 }
