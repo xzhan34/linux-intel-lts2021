d19e5002e2e2a269c80a7b658c7b87479f0c321e
Umesh Nerlige Ramappa <umesh.nerlige.ramappa@intel.com>
2021-02-17 11:18:16 -0800

    INTEL_DII: i915/perf: Enable OA formats for PVC
    Enable OA formats for PVC.
    
    v2: Rebase from DG2 fixes (Umesh)
    
    Signed-off-by: Umesh Nerlige Ramappa <umesh.nerlige.ramappa@intel.com>
    Reviewed-by: Ashutosh Dixit <ashutosh.dixit@intel.com>

--- drivers/gpu/drm/i915/i915_perf.c
+++ drivers/gpu/drm/i915/i915_perf.c
@@ -5795,6 +5795,7 @@ static void oa_init_supported_formats(struct i915_perf *perf)
 		break;
 
 	case INTEL_DG2:
+	case INTEL_PONTEVECCHIO:
 		oa_format_add(perf, I915_OAR_FORMAT_A32u40_A4u32_B8_C8);
 		oa_format_add(perf, I915_OA_FORMAT_A24u40_A14u32_B8_C8);
 		oa_format_add(perf, PRELIM_I915_OAR_FORMAT_A32u40_A4u32_B8_C8);
