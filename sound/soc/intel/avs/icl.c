754a36436796dd9b189c52dc82a5968ce578a7d0
Cezary Rojewski <cezary.rojewski@intel.com>
2021-09-15 16:25:20 +0200

    ANDROID: ASoC: Intel: avs: ICL-based platforms support
    Define handlers specific to cAVS 1.5 platforms, that is ICL, JSL and all
    other variants based on this very version of AudioDSP architecture. Most
    operations are inherited from their predecessors with the major
    difference being firmware-logging functionality - IPC request as well as
    debug memory windows layout have changed.
    
    Change-Id: I8d7ffe60b03e0d6446afd5010fb713678256faaa
    Signed-off-by: Cezary Rojewski <cezary.rojewski@intel.com>

---
+++ sound/soc/intel/avs/icl.c
@@ -0,0 +1,135 @@
+// SPDX-License-Identifier: GPL-2.0-only
+//
+// Copyright(c) 2021 Intel Corporation. All rights reserved.
+//
+// Authors: Cezary Rojewski <cezary.rojewski@intel.com>
+//          Amadeusz Slawinski <amadeuszx.slawinski@linux.intel.com>
+//
+
+#include <linux/slab.h>
+#include "avs.h"
+#include "messages.h"
+
+static int icl_enable_logs(struct avs_dev *adev, enum avs_log_enable enable, u32 aging_period,
+			   u32 fifo_full_period, unsigned long resource_mask, u32 *priorities)
+{
+	struct icl_log_state_info *info;
+	u32 size, num_libs = adev->fw_cfg.max_libs_count;
+	int i, ret;
+
+	if (fls_long(resource_mask) > num_libs)
+		return -EINVAL;
+	size = struct_size(info, logs_priorities_mask, num_libs);
+	info = kzalloc(size, GFP_KERNEL);
+	if (!info)
+		return -ENOMEM;
+
+	info->aging_timer_period = aging_period;
+	info->fifo_full_timer_period = fifo_full_period;
+	info->enable = enable;
+	if (enable)
+		for_each_set_bit(i, &resource_mask, num_libs)
+			info->logs_priorities_mask[i] = *priorities++;
+
+	ret = avs_ipc_set_enable_logs(adev, (u8 *)info, size);
+	kfree(info);
+	if (ret)
+		return AVS_IPC_RET(ret);
+
+	return 0;
+}
+
+union icl_memwnd2_slot_type {
+	u32 val;
+	struct {
+		u32 resource_id:8;
+		u32 type:24;
+	};
+};
+
+struct icl_memwnd2_desc {
+	u32 resource_id;
+	union icl_memwnd2_slot_type slot_id;
+	u32 vma;
+} __packed;
+
+#define ICL_MEMWND2_SLOTS_COUNT	15
+
+struct icl_memwnd2 {
+	union {
+		struct icl_memwnd2_desc slot_desc[ICL_MEMWND2_SLOTS_COUNT];
+		u8 rsvd[PAGE_SIZE];
+	};
+	u8 slot_array[ICL_MEMWND2_SLOTS_COUNT][PAGE_SIZE];
+} __packed;
+
+#define ICL_SLOT_UNUSED \
+	((union icl_memwnd2_slot_type) { 0x00000000U })
+#define ICL_SLOT_CRITICAL_LOG \
+	((union icl_memwnd2_slot_type) { 0x54524300U })
+#define ICL_SLOT_DEBUG_LOG \
+	((union icl_memwnd2_slot_type) { 0x474f4c00U })
+#define ICL_SLOT_GDB_STUB \
+	((union icl_memwnd2_slot_type) { 0x42444700U })
+#define ICL_SLOT_BROKEN \
+	((union icl_memwnd2_slot_type) { 0x44414544U })
+
+static int icl_slot_offset(struct avs_dev *adev, union icl_memwnd2_slot_type slot_type)
+{
+	struct icl_memwnd2_desc desc[ICL_MEMWND2_SLOTS_COUNT];
+	int i;
+
+	memcpy_fromio(&desc, avs_sram_addr(adev, AVS_DEBUG_WINDOW), sizeof(desc));
+
+	for (i = 0; i < ICL_MEMWND2_SLOTS_COUNT; i++)
+		if (desc[i].slot_id.val == slot_type.val)
+			return offsetof(struct icl_memwnd2, slot_array) +
+			       skl_log_buffer_offset(adev, i);
+	return -ENXIO;
+}
+
+static int icl_log_buffer_offset(struct avs_dev *adev, u32 core)
+{
+	union icl_memwnd2_slot_type slot_type = ICL_SLOT_DEBUG_LOG;
+	int ret;
+
+	slot_type.resource_id = core;
+	ret = icl_slot_offset(adev, slot_type);
+	if (ret < 0)
+		dev_dbg(adev->dev, "No slot offset found for: %x\n",
+			slot_type.val);
+
+	return ret;
+}
+
+static bool icl_d0ix_toggle(struct avs_dev *adev, struct avs_ipc_msg *tx, bool wake)
+{
+	/* payload-less IPCs do not take part in d0ix toggling */
+	return tx->size;
+}
+
+static int icl_set_d0ix(struct avs_dev *adev, bool enable)
+{
+	int ret;
+
+	ret = avs_ipc_set_d0ix(adev, enable, false);
+	return AVS_IPC_RET(ret);
+}
+
+const struct avs_dsp_ops icl_dsp_ops = {
+	.power = avs_dsp_core_power,
+	.reset = avs_dsp_core_reset,
+	.stall = avs_dsp_core_stall,
+	.irq_handler = avs_ipc_irq_handler,
+	.irq_thread = cnl_ipc_irq_thread,
+	.int_control = avs_dsp_interrupt_control,
+	.load_basefw = avs_hda_load_basefw,
+	.load_lib = avs_hda_load_library,
+	.transfer_mods = avs_hda_transfer_modules,
+	.log_buffer_offset = icl_log_buffer_offset,
+	.log_buffer_status = apl_log_buffer_status,
+	.coredump = apl_coredump,
+	.d0ix_toggle = icl_d0ix_toggle,
+	.set_d0ix = icl_set_d0ix,
+	AVS_SET_ENABLE_LOGS_OP(icl)
+};
