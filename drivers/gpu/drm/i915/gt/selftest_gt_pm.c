a5ac665a32a3f6fc4d5f662b64c9b36a0d759b98
Sujaritha Sundaresan <sujaritha.sundaresan@intel.com>
2022-03-31 10:34:19 -0700

    INTEL_DII/FIXME: drm/i915/xehpsdv: Run live gt pm selftest on all GTs
    Adding support to run live_gt_pm selftest on all GTs.
    Sending this as a FIXME since the permanent fix to pull the iteration
    over all GT into a central test runner is currently in progress.
    
    v2: Remove redundant intel_gt_is_wedged check
    
    Signed-off-by: Sujaritha Sundaresan <sujaritha.sundaresan@intel.com>
    Reviewed-by: Andi Shyti <andi.shyti@linux.intel.com>

--- drivers/gpu/drm/i915/gt/selftest_gt_pm.c
+++ drivers/gpu/drm/i915/gt/selftest_gt_pm.c
@@ -195,10 +195,18 @@ int intel_gt_pm_live_selftests(struct drm_i915_private *i915)
 		SUBTEST(live_gt_resume),
 	};
 
-	if (intel_gt_is_wedged(to_gt(i915)))
-		return 0;
+	struct intel_gt *gt;
+	unsigned int i;
 
-	return intel_gt_live_subtests(tests, to_gt(i915));
+	for_each_gt(gt, i915, i) {
+		int ret;
+
+		ret = intel_gt_live_subtests(tests, gt);
+		if (ret)
+			return ret;
+	}
+
+	return 0;
 }
 
 int intel_gt_pm_late_selftests(struct drm_i915_private *i915)
