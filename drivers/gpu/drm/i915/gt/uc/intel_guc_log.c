aec26702e16c4de86f6187729daf48242b145ff3
John Harrison <John.C.Harrison@Intel.com>
2022-06-17 08:47:09 -0700

    INTEL_DII/NOT_UPSTREAM: drm/i915/guc: Make GuC log sizes runtime configurable
    The GuC log buffer sizes had to be configured statically at compile
    time. This can be quite troublesome when needing to get larger logs
    out of a released driver. So re-organise the code to allow a boot time
    module parameter override.
    
    v2: Rebase after partial upstream. Apparently, adding module
    parameters for log sizes is absolutely forbidden in upstream. So this
    must stay as an internal only patch.
    
    Signed-off-by: John Harrison <John.C.Harrison@Intel.com>
    Reviewed-by: Umesh Nerlige Ramappa <umesh.nerlige.ramappa@intel.com>

--- drivers/gpu/drm/i915/gt/uc/intel_guc_log.c
+++ drivers/gpu/drm/i915/gt/uc/intel_guc_log.c
@@ -36,6 +36,24 @@ struct guc_log_section {
 	const char *name;
 };
 
+static s32 scale_log_param(struct intel_guc_log *log, const struct guc_log_section *section,
+			   s32 param)
+{
+	/* -1 means default */
+	if (param < 0)
+		return section->default_val;
+
+	/* Check for 32-bit overflow */
+	if (param >= SZ_4K) {
+		drm_err(&guc_to_gt(log_to_guc(log))->i915->drm, "Size too large for GuC %s log: %dMB!",
+			section->name, param);
+		return section->default_val;
+	}
+
+	/* Param units are 1MB */
+	return param * SZ_1M;
+}
+
 static void _guc_log_init_sizes(struct intel_guc_log *log)
 {
 	struct intel_guc *guc = log_to_guc(log);
@@ -60,14 +78,20 @@ static void _guc_log_init_sizes(struct intel_guc_log *log)
 			"capture",
 		}
 	};
+	s32 params[GUC_LOG_SECTIONS_LIMIT] = {
+		i915->params.guc_log_size_crash,
+		i915->params.guc_log_size_debug,
+		i915->params.guc_log_size_capture,
+	};
 	int i;
 
 	for (i = 0; i < GUC_LOG_SECTIONS_LIMIT; i++)
-		log->sizes[i].bytes = sections[i].default_val;
+		log->sizes[i].bytes = scale_log_param(log, sections + i, params[i]);
 
 	/* If debug size > 1MB then bump default crash size to keep the same units */
-	if (log->sizes[GUC_LOG_SECTIONS_DEBUG].bytes >= SZ_1M &&
-	    GUC_LOG_DEFAULT_CRASH_BUFFER_SIZE < SZ_1M)
+	if ((log->sizes[GUC_LOG_SECTIONS_DEBUG].bytes >= SZ_1M) &&
+	    (i915->params.guc_log_size_crash == -1) &&
+	    (GUC_LOG_DEFAULT_CRASH_BUFFER_SIZE < SZ_1M))
 		log->sizes[GUC_LOG_SECTIONS_CRASH].bytes = SZ_1M;
 
 	/* Prepare the GuC API structure fields: */
