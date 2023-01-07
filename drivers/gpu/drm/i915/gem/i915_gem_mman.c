torvalds/linux

commit b97060a99b01b4d706b87df450b69f82962d2fba

drm/i915/guc: Update intel_gt_wait_for_idle to work with GuC
When running the GuC the GPU can't be considered idle if the GuC still
has contexts pinned. As such, a call has been added in
intel_gt_wait_for_idle to idle the UC and in turn the GuC by waiting for
the number of unpinned contexts to go to zero.

v2: rtimeout -> remaining_timeout
v3: Drop unnecessary includes, guc_submission_busy_loop ->
guc_submission_send_busy_loop, drop negatie timeout trick, move a
refactor of guc_context_unpin to earlier path (John H)
v4: Add stddef.h back into intel_gt_requests.h, sort circuit idle
function if not in GuC submission mode

Cc: John Harrison <john.c.harrison@intel.com>
Signed-off-by: Matthew Brost <matthew.brost@intel.com>
Reviewed-by: John Harrison <John.C.Harrison@Intel.com>
Signed-off-by: John Harrison <John.C.Harrison@Intel.com>
Link: https://patchwork.freedesktop.org/patch/msgid/20210721215101.139794-16-matthew.brost@intel.com

drivers/gpu/drm/i915/gem/i915_gem_mman.c
@@ -645,7 +645,8 @@ mmap_offset_attach(struct drm_i915_gem_object *obj,
 		goto insert;
 
 	/* Attempt to reap some mmap space from dead objects */
-	err = intel_gt_retire_requests_timeout(&i915->gt, MAX_SCHEDULE_TIMEOUT);
+	err = intel_gt_retire_requests_timeout(&i915->gt, MAX_SCHEDULE_TIMEOUT,
+					       NULL);
 	if (err)
 		goto err;
 

drivers/gpu/drm/i915/gt/intel_gt.c
@@ -585,6 +585,25 @@ static void __intel_gt_disable(struct intel_gt *gt)
 	GEM_BUG_ON(intel_gt_pm_is_awake(gt));
 }
 
+int intel_gt_wait_for_idle(struct intel_gt *gt, long timeout)
+{
+	long remaining_timeout;
+
+	/* If the device is asleep, we have no requests outstanding */
+	if (!intel_gt_pm_is_awake(gt))
+		return 0;
+
+	while ((timeout = intel_gt_retire_requests_timeout(gt, timeout,
+							   &remaining_timeout)) > 0) {
+		cond_resched();
+		if (signal_pending(current))
+			return -EINTR;
+	}
+
+	return timeout ? timeout : intel_uc_wait_for_idle(&gt->uc,
+							  remaining_timeout);
+}
+
 int intel_gt_init(struct intel_gt *gt)
 {
 	int err;

drivers/gpu/drm/i915/gt/intel_gt_requests.c
@@ -130,7 +130,8 @@ void intel_engine_fini_retire(struct intel_engine_cs *engine)
 	GEM_BUG_ON(engine->retire);
 }
 
-long intel_gt_retire_requests_timeout(struct intel_gt *gt, long timeout)
+long intel_gt_retire_requests_timeout(struct intel_gt *gt, long timeout,
+				      long *remaining_timeout)
 {
 	struct intel_gt_timelines *timelines = &gt->timelines;
 	struct intel_timeline *tl, *tn;
@@ -195,22 +196,10 @@ out_active:	spin_lock(&timelines->lock);
 	if (flush_submission(gt, timeout)) /* Wait, there's more! */
 		active_count++;
 
+	if (remaining_timeout)
+		*remaining_timeout = timeout;
+
 	return active_count ? timeout : 0;
-}
-
-int intel_gt_wait_for_idle(struct intel_gt *gt, long timeout)
-{
-	/* If the device is asleep, we have no requests outstanding */
-	if (!intel_gt_pm_is_awake(gt))
-		return 0;
-
-	while ((timeout = intel_gt_retire_requests_timeout(gt, timeout)) > 0) {
-		cond_resched();
-		if (signal_pending(current))
-			return -EINTR;
-	}
-
-	return timeout;
 }
 
 static void retire_work_handler(struct work_struct *work)

drivers/gpu/drm/i915/gt/intel_gt_requests.h
@@ -6,22 +6,23 @@
 #ifndef INTEL_GT_REQUESTS_H
 #define INTEL_GT_REQUESTS_H
 
+#include <stddef.h>
+
 struct intel_engine_cs;
 struct intel_gt;
 struct intel_timeline;
 
-long intel_gt_retire_requests_timeout(struct intel_gt *gt, long timeout);
+long intel_gt_retire_requests_timeout(struct intel_gt *gt, long timeout,
+				      long *remaining_timeout);
 static inline void intel_gt_retire_requests(struct intel_gt *gt)
 {
-	intel_gt_retire_requests_timeout(gt, 0);
+	intel_gt_retire_requests_timeout(gt, 0, NULL);
 }
 
 void intel_engine_init_retire(struct intel_engine_cs *engine);
 void intel_engine_add_retire(struct intel_engine_cs *engine,
 			     struct intel_timeline *tl);
 void intel_engine_fini_retire(struct intel_engine_cs *engine);
-
-int intel_gt_wait_for_idle(struct intel_gt *gt, long timeout);
 
 void intel_gt_init_requests(struct intel_gt *gt);
 void intel_gt_park_requests(struct intel_gt *gt);

drivers/gpu/drm/i915/selftests/mock_gem_device.c
@@ -52,7 +52,8 @@ void mock_device_flush(struct drm_i915_private *i915)
 	do {
 		for_each_engine(engine, gt, id)
 			mock_engine_flush(engine);
-	} while (intel_gt_retire_requests_timeout(gt, MAX_SCHEDULE_TIMEOUT));
+	} while (intel_gt_retire_requests_timeout(gt, MAX_SCHEDULE_TIMEOUT,
+						  NULL));
 }
 
 static void mock_device_release(struct drm_device *dev)

drivers/gpu/drm/i915/gt/intel_gt.h
@@ -48,6 +48,8 @@ void intel_gt_driver_release(struct intel_gt *gt);
 
 void intel_gt_driver_late_release(struct intel_gt *gt);
 
+int intel_gt_wait_for_idle(struct intel_gt *gt, long timeout);
+
 void intel_gt_check_and_clear_faults(struct intel_gt *gt);
 void intel_gt_clear_error_registers(struct intel_gt *gt,
 				    intel_engine_mask_t engine_mask);

drivers/gpu/drm/i915/gt/uc/intel_guc.h
@@ -39,6 +39,8 @@ struct intel_guc {
 	spinlock_t irq_lock;
 	unsigned int msg_enabled_mask;
 
+	atomic_t outstanding_submission_g2h;
+
 	struct {
 		void (*reset)(struct intel_guc *guc);
 		void (*enable)(struct intel_guc *guc);
@@ -244,6 +246,8 @@ static inline void intel_guc_disable_msg(struct intel_guc *guc, u32 mask)
 	guc->msg_enabled_mask &= ~mask;
 	spin_unlock_irq(&guc->irq_lock);
 }
+
+int intel_guc_wait_for_idle(struct intel_guc *guc, long timeout);
 
 int intel_guc_reset_engine(struct intel_guc *guc,
 			   struct intel_engine_cs *engine);

drivers/gpu/drm/i915/gt/uc/intel_guc_ct.c
@@ -109,6 +109,7 @@ void intel_guc_ct_init_early(struct intel_guc_ct *ct)
 	INIT_LIST_HEAD(&ct->requests.incoming);
 	INIT_WORK(&ct->requests.worker, ct_incoming_request_worker_func);
 	tasklet_setup(&ct->receive_tasklet, ct_receive_tasklet_func);
+	init_waitqueue_head(&ct->wq);
 }
 
 static inline const char *guc_ct_buffer_type_to_str(u32 type)

xzhan34/linux-intel-lts2021

commit f5bbf84fe9f99e0fb17965c4620f058397d780ac

drm/i915/gt: Handle EINTR cleanly for intel_uc_wait_for_idle
Prevent forward propagation of the interrupted
intel_gt_retire_requests_timeout() into intel_uc_wait_for_idle(),
passing a negative timeout.

Fixes: b97060a99b01 ("drm/i915/guc: Update intel_gt_wait_for_idle to work with GuC")
Signed-off-by: Chris Wilson <chris.p.wilson@linux.intel.com>
Acked-by: Mika Kuoppala <mika.kuoppala@linux.intel.com>

drivers/gpu/drm/i915/gem/i915_gem_mman.c
@@ -672,7 +672,9 @@ mmap_offset_attach(struct drm_i915_gem_object *obj,
 {
 	struct drm_i915_private *i915 = to_i915(obj->base.dev);
 	struct i915_mmap_offset *mmo;
+	struct intel_gt *gt;
 	int err;
+	int i;
 
 	GEM_BUG_ON(obj->ops->mmap_offset || obj->ops->mmap_ops);
 
@@ -694,12 +696,10 @@ mmap_offset_attach(struct drm_i915_gem_object *obj,
 		goto insert;
 
 	/* Attempt to reap some mmap space from dead objects */
-	err = intel_gt_retire_requests_timeout(to_gt(i915), MAX_SCHEDULE_TIMEOUT,
-					       NULL);
-	if (err)
-		goto err;
-
+	for_each_gt(gt, i915, i)
+		intel_gt_retire_requests(gt);
 	i915_gem_drain_freed_objects(i915);
+
 	err = drm_vma_offset_add(obj->base.dev->vma_offset_manager,
 				 &mmo->vma_node, obj->base.size / PAGE_SIZE);
 	if (err)

drivers/gpu/drm/i915/gt/intel_gt.c
@@ -552,7 +552,7 @@ err:
 	}
 
 	/* Flush the default context image to memory, and enable powersaving. */
-	if (intel_gt_wait_for_idle(gt, I915_GEM_IDLE_TIMEOUT) == -ETIME) {
+	if (intel_gt_wait_for_idle(gt, I915_GEM_IDLE_TIMEOUT)) {
 		err = -EIO;
 		goto out;
 	}
@@ -589,20 +589,15 @@ out:
 	 * and ready to be torn-down. The quickest way we can accomplish
 	 * this is by declaring ourselves wedged.
 	 */
-	if (err)
-		intel_gt_set_wedged(gt);
-
 	for (id = 0; id < ARRAY_SIZE(requests); id++) {
-		struct intel_context *ce;
 		struct i915_request *rq;
 
 		rq = requests[id];
 		if (!rq)
 			continue;
 
-		ce = rq->context;
+		intel_context_put(rq->context);
 		i915_request_put(rq);
-		intel_context_put(ce);
 	}
 	return err;
 }
@@ -622,7 +617,7 @@ static int __engines_verify_workarounds(struct intel_gt *gt)
 	}
 
 	/* Flush and restore the kernel context for safety */
-	if (intel_gt_wait_for_idle(gt, I915_GEM_IDLE_TIMEOUT) == -ETIME)
+	if (intel_gt_wait_for_idle(gt, I915_GEM_IDLE_TIMEOUT))
 		err = -EIO;
 
 	return err;
@@ -640,21 +635,21 @@ static void __intel_gt_disable(struct intel_gt *gt)
 
 int intel_gt_wait_for_idle(struct intel_gt *gt, long timeout)
 {
-	long remaining_timeout;
-
 	/* If the device is asleep, we have no requests outstanding */
 	if (!intel_gt_pm_is_awake(gt))
 		return 0;
 
-	while ((timeout = intel_gt_retire_requests_timeout(gt, timeout,
-							   &remaining_timeout)) > 0) {
-		cond_resched();
+	while (!intel_gt_retire_requests_timeout(gt, &timeout)) {
 		if (signal_pending(current))
 			return -EINTR;
+
+		if (timeout == 0)
+			return -ETIME;
+
+		cond_resched();
 	}
 
-	return timeout ? timeout : intel_uc_wait_for_idle(&gt->uc,
-							  remaining_timeout);
+	return intel_uc_wait_for_idle(&gt->uc, timeout);
 }
 
 int intel_gt_init(struct intel_gt *gt)

drivers/gpu/drm/i915/gt/intel_gt_requests.c
@@ -8,42 +8,53 @@
 #include "i915_drv.h" /* for_each_engine() */
 #include "i915_request.h"
 #include "intel_engine_heartbeat.h"
+#include "intel_engine_pm.h"
 #include "intel_execlists_submission.h"
 #include "intel_gt.h"
 #include "intel_gt_pm.h"
 #include "intel_gt_requests.h"
 #include "intel_timeline.h"
 
-static bool retire_requests(struct intel_timeline *tl)
+static void retire_requests(const struct intel_timeline *tl)
 {
 	struct i915_request *rq, *rn;
 
 	list_for_each_entry_safe(rq, rn, &tl->requests, link)
 		if (!i915_request_retire(rq))
-			return false;
-
-	/* And check nothing new was submitted */
-	return !i915_active_fence_isset(&tl->last_request);
+			return;
 }
 
-static bool engine_active(const struct intel_engine_cs *engine)
+static bool timeline_retire(struct intel_timeline *tl)
 {
-	return !list_empty(&engine->kernel_context->timeline->requests);
+	if (list_empty(&tl->requests))
+		return true;
+
+	if (mutex_trylock(&tl->mutex)) {
+		retire_requests(tl);
+		mutex_unlock(&tl->mutex);
+	}
+
+	return list_empty(&tl->requests);
 }
 
-static bool flush_submission(struct intel_gt *gt, long timeout)
+static bool engine_idle(const struct intel_engine_cs *engine)
+{
+	return timeline_retire(engine->kernel_context->timeline);
+}
+
+static bool flush_submission(struct intel_gt *gt)
 {
 	struct intel_engine_cs *engine;
 	enum intel_engine_id id;
-	bool active = false;
-
-	if (!timeout)
-		return false;
+	bool idle = true;
 
 	if (!intel_gt_pm_is_awake(gt))
-		return false;
+		return true;
 
 	for_each_engine(engine, gt, id) {
+		if (!intel_engine_pm_is_awake(engine))
+			continue;
+
 		intel_engine_flush_submission(engine);
 
 		/* Flush the background retirement and idle barriers */
@@ -51,10 +62,11 @@ static bool flush_submission(struct intel_gt *gt, long timeout)
 		flush_delayed_work(&engine->wakeref.work);
 
 		/* Is the idle barrier still outstanding? */
-		active |= engine_active(engine);
+		if (engine->kernel_context)
+			idle &= engine_idle(engine);
 	}
 
-	return active;
+	return idle;
 }
 
 static void engine_retire(struct work_struct *work)
@@ -130,14 +142,27 @@ void intel_engine_fini_retire(struct intel_engine_cs *engine)
 	GEM_BUG_ON(engine->retire);
 }
 
-long intel_gt_retire_requests_timeout(struct intel_gt *gt, long timeout,
-				      long *remaining_timeout)
+/**
+ * intel_gt_retire_requests_timeout - try to retire all completed requests
+ * @gt - the GT from which to retire outstanding requests
+ * @remain - How long we should wait in total for any outstanding request
+ *
+ * Look through all the active timelines/contexts on the GT, and try to
+ * retire all the requests. If @remain is provided, any incomplete
+ * timeline will be waited on upto the remaining timeout (in jiffies).
+ * Any time not consumed by waiting is returned via @remain.
+ *
+ * Returns true if all requests were retired and the GT is now idle.
+ */
+bool intel_gt_retire_requests_timeout(struct intel_gt *gt, long *remain)
 {
 	struct intel_gt_timelines *timelines = &gt->timelines;
-	unsigned long active_count = 0;
+	long timeout = remain ? *remain : -ETIME;
 	struct intel_timeline *tl;
+	bool idle = true;
 
-	flush_submission(gt, timeout); /* kick the ksoftirqd tasklets */
+	if (remain)
+		flush_submission(gt); /* kick the ksoftirqd tasklets */
 
 	rcu_read_lock();
 	list_for_each_entry_rcu(tl, &timelines->active_list, link) {
@@ -158,27 +183,19 @@ long intel_gt_retire_requests_timeout(struct intel_gt *gt, long timeout,
 		}
 
 		/* Retirement is best effort */
-		if (!list_empty(&tl->requests)) {
-			if (mutex_trylock(&tl->mutex)) {
-				active_count += !retire_requests(tl);
-				mutex_unlock(&tl->mutex);
-			} else {
-				active_count++;
-			}
-		}
+		idle &= timeline_retire(tl);
 
 		rcu_read_lock();
 		intel_timeline_put_active(tl);
 	}
 	rcu_read_unlock();
 
-	if (flush_submission(gt, timeout)) /* Wait, there's more! */
-		active_count++;
+	if (remain) {
+		idle &= flush_submission(gt); /* Wait, there's more! */
+		*remain = max(timeout, 0l);
+	}
 
-	if (remaining_timeout)
-		*remaining_timeout = timeout;
-
-	return active_count ? timeout : 0;
+	return idle;
 }
 
 static void retire_work_handler(struct work_struct *work)

drivers/gpu/drm/i915/gt/intel_gt_requests.h
@@ -6,17 +6,16 @@
 #ifndef INTEL_GT_REQUESTS_H
 #define INTEL_GT_REQUESTS_H
 
-#include <stddef.h>
+#include <linux/types.h>
 
 struct intel_engine_cs;
 struct intel_gt;
 struct intel_timeline;
 
-long intel_gt_retire_requests_timeout(struct intel_gt *gt, long timeout,
-				      long *remaining_timeout);
+bool intel_gt_retire_requests_timeout(struct intel_gt *gt, long *timeout);
 static inline void intel_gt_retire_requests(struct intel_gt *gt)
 {
-	intel_gt_retire_requests_timeout(gt, 0, NULL);
+	intel_gt_retire_requests_timeout(gt, NULL);
 }
 
 void intel_engine_init_retire(struct intel_engine_cs *engine);

drivers/gpu/drm/i915/gt/intel_reset.c
@@ -1512,10 +1512,15 @@ void intel_gt_set_wedged_on_init(struct intel_gt *gt)
 
 void intel_gt_set_wedged_on_fini(struct intel_gt *gt)
 {
+	long timeout = 0;
+
 	intel_gt_set_wedged(gt);
 	i915_disable_error_state(gt->i915, -ENODEV);
 	set_bit(I915_WEDGED_ON_FINI, &gt->reset.flags);
-	intel_gt_retire_requests(gt); /* cleanup any wedged requests */
+
+	/* cleanup any wedged requests */
+	while (!intel_gt_retire_requests_timeout(gt, &timeout))
+		;
 }
 
 void intel_gt_init_reset(struct intel_gt *gt)

drivers/gpu/drm/i915/i915_debugfs.c
@@ -772,10 +772,11 @@ i915_drop_caches_get(void *data, u64 *val)
 static int
 gt_drop_caches(struct intel_gt *gt, u64 val)
 {
+	long timeout = I915_IDLE_ENGINES_TIMEOUT;
 	int ret;
 
 	if (val & DROP_RESET_ACTIVE &&
-	    intel_gt_wait_for_idle(gt, I915_IDLE_ENGINES_TIMEOUT) == -ETIME)
+	    !intel_gt_retire_requests_timeout(gt, &timeout))
 		intel_gt_set_wedged(gt);
 
 	if (val & DROP_RETIRE)

drivers/gpu/drm/i915/i915_request.c
@@ -104,9 +104,9 @@ static signed long i915_fence_wait(struct dma_fence *fence,
 				   bool interruptible,
 				   signed long timeout)
 {
-	return i915_request_wait_timeout(to_request(fence),
-					 interruptible | I915_WAIT_PRIORITY,
-					 timeout);
+	return i915_request_wait(to_request(fence),
+				 interruptible | I915_WAIT_PRIORITY,
+				 timeout) ?: 0;
 }
 
 struct kmem_cache *i915_request_slab_cache(void)
@@ -1940,12 +1940,12 @@ static void request_wait_wake(struct dma_fence *fence, struct dma_fence_cb *cb)
 }
 
 /**
- * i915_request_wait_timeout - wait until execution of request has finished
+ * i915_request_wait - wait until execution of request has finished
  * @rq: the request to wait upon
  * @flags: how to wait
  * @timeout: how long to wait in jiffies
  *
- * i915_request_wait_timeout() waits for the request to be completed, for a
+ * i915_request_wait() waits for the request to be completed, for a
  * maximum of @timeout jiffies (with MAX_SCHEDULE_TIMEOUT implying an
  * unbounded wait).
  *
@@ -1955,12 +1955,10 @@ static void request_wait_wake(struct dma_fence *fence, struct dma_fence_cb *cb)
  *
  * May return -EINTR is called with I915_WAIT_INTERRUPTIBLE and a signal is
  * pending before the request completes.
- *
- * NOTE: This function has the same wait semantics as dma-fence.
  */
-long i915_request_wait_timeout(struct i915_request *rq,
-			       unsigned int flags,
-			       long timeout)
+long i915_request_wait(struct i915_request *rq,
+		       unsigned int flags,
+		       long timeout)
 {
 	const int state = flags & I915_WAIT_INTERRUPTIBLE ?
 		TASK_INTERRUPTIBLE : TASK_UNINTERRUPTIBLE;
@@ -1970,7 +1968,7 @@ long i915_request_wait_timeout(struct i915_request *rq,
 	GEM_BUG_ON(timeout < 0);
 
 	if (dma_fence_is_signaled(&rq->fence))
-		return timeout ?: 1;
+		return timeout;
 
 	if (!timeout)
 		return -ETIME;
@@ -2077,39 +2075,6 @@ out:
 	mutex_release(&rq->engine->gt->reset.mutex.dep_map, _THIS_IP_);
 	trace_i915_request_wait_end(rq);
 	return timeout;
-}
-
-/**
- * i915_request_wait - wait until execution of request has finished
- * @rq: the request to wait upon
- * @flags: how to wait
- * @timeout: how long to wait in jiffies
- *
- * i915_request_wait() waits for the request to be completed, for a
- * maximum of @timeout jiffies (with MAX_SCHEDULE_TIMEOUT implying an
- * unbounded wait).
- *
- * Returns the remaining time (in jiffies) if the request completed, which may
- * be zero or -ETIME if the request is unfinished after the timeout expires.
- * May return -EINTR is called with I915_WAIT_INTERRUPTIBLE and a signal is
- * pending before the request completes.
- *
- * NOTE: This function behaves differently from dma-fence wait semantics for
- * timeout = 0. It returns 0 on success, and -ETIME if not signaled.
- */
-long i915_request_wait(struct i915_request *rq,
-		       unsigned int flags,
-		       long timeout)
-{
-	long ret = i915_request_wait_timeout(rq, flags, timeout);
-
-	if (!ret)
-		return -ETIME;
-
-	if (ret > 0 && !timeout)
-		return 0;
-
-	return ret;
 }
 
 static int print_sched_attr(const struct i915_sched_attr *attr,

drivers/gpu/drm/i915/i915_request.h
@@ -417,11 +417,6 @@ void i915_request_unsubmit(struct i915_request *request);
 
 void i915_request_cancel(struct i915_request *rq, int error);
 
-long i915_request_wait_timeout(struct i915_request *rq,
-			       unsigned int flags,
-			       long timeout)
-	__attribute__((nonnull(1)));
-
 long i915_request_wait(struct i915_request *rq,
 		       unsigned int flags,
 		       long timeout)

drivers/gpu/drm/i915/selftests/i915_request.c
@@ -1730,15 +1730,7 @@ static int switch_to_kernel_sync(struct intel_context *ce, int err)
 		err = -ETIME;
 	i915_request_put(rq);
 
-	if (intel_gt_retire_requests_timeout(engine->gt, HZ, NULL) < 0) {
-		struct drm_printer p = drm_info_printer(engine->i915->drm.dev);
-
-		pr_err("%s: Failed to idle iactive engine\n", engine->name);
-		intel_engine_dump(engine, &p, "%s\n", engine->name);
-		err = -EIO;
-	}
 	intel_gt_retire_requests(engine->gt);
-
 	return err;
 }
 

drivers/gpu/drm/i915/selftests/mock_gem_device.c
@@ -45,6 +45,7 @@
 
 void mock_device_flush(struct drm_i915_private *i915)
 {
+	long timeout = MAX_SCHEDULE_TIMEOUT;
 	struct intel_gt *gt = to_gt(i915);
 	struct intel_engine_cs *engine;
 	enum intel_engine_id id;
@@ -52,8 +53,7 @@ void mock_device_flush(struct drm_i915_private *i915)
 	do {
 		for_each_engine(engine, gt, id)
 			mock_engine_flush(engine);
-	} while (intel_gt_retire_requests_timeout(gt, MAX_SCHEDULE_TIMEOUT,
-						  NULL));
+	} while (!intel_gt_retire_requests_timeout(gt, &timeout));
 }
 
 static void mock_device_release(struct drm_device *dev)