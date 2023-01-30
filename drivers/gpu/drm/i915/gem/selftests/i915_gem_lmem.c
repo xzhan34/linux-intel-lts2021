0d9ad6607696e80ad7bf922f5eb3f78b2e702edb
Chris Wilson <chris.p.wilson@linux.intel.com>
2023-01-30 10:17:15 +0000

    INTEL_DII/NOT_UPSTREAM: drm/i915/gem: Compare lmem clear against expected bandwidth
    Check that we reach ~90% of the expected (usually experimentally
    derived) lmem write bandwidth with our blitter clears.
    
    Signed-off-by: Chris Wilson <chris.p.wilson@linux.intel.com>

--- drivers/gpu/drm/i915/gem/selftests/i915_gem_lmem.c
+++ drivers/gpu/drm/i915/gem/selftests/i915_gem_lmem.c
@@ -5,14 +5,27 @@
 
 #include <linux/pm_qos.h>
 
+#include <drm/i915_pciids.h>
+
 #include "gt/intel_gt_clock_utils.h"
 #include "gt/intel_rps.h"
 
 #include "i915_selftest.h"
+#include "intel_pcode.h"
 #include "selftests/i915_random.h"
 #include "selftests/igt_flush_test.h"
 
 #define CPU_LATENCY 0 /* -1 to disable pm_qos, 0 to disable cstates */
+
+#define _MBs(x) (((x) * 1000000ull) >> 20)
+#define MBs(x) (void *)_MBs(x)
+static const struct pci_device_id clear_bandwidth[] = {
+	INTEL_DG1_IDS(MBs(66000)),
+	INTEL_DG2_G10_IDS(MBs(360000)),
+	INTEL_DG2_G11_IDS(MBs(33000)),
+	INTEL_DG2_G12_IDS(MBs(250000)),
+	{},
+};
 
 static int igt_lmem_touch(void *arg)
 {
@@ -30,10 +43,9 @@ static int igt_lmem_touch(void *arg)
 	return 0;
 }
 
-static int igt_lmem_clear(void *arg)
+static int __igt_lmem_clear(struct drm_i915_private *i915, bool measure)
 {
 	const u64 poison = make_u64(0xc5c55c5c, 0xa3a33a3a);
-	struct drm_i915_private *i915 = arg;
 	struct pm_qos_request qos;
 	struct sg_table *pages;
 	struct intel_gt *gt;
@@ -55,6 +67,7 @@ static int igt_lmem_clear(void *arg)
 
 	for_each_gt(gt, i915, id) {
 		struct intel_context *ce;
+		unsigned int max_bw = 0;
 		intel_wakeref_t wf;
 		u64 size;
 
@@ -119,8 +132,11 @@ static int igt_lmem_clear(void *arg)
 				gpu_bw = div_u64(mul_u64_u32_shr(sz, NSEC_PER_SEC, 20), gpu);
 
 				dev_info(gt->i915->drm.dev,
-					 "GT%d: checked with size:%x, CPU write:%dMiB/s, GPU write:%dMiB/s, overhead:%lldns\n",
-					 id, sz, cpu_bw, gpu_bw, sync - gpu);
+					 "GT%d: checked with size:%x, CPU write:%dMiB/s, GPU write:%dMiB/s, overhead:%lldns, freq:%dMHz\n",
+					 id, sz, cpu_bw, gpu_bw, sync - gpu,
+					 intel_rps_read_actual_frequency(&gt->rps));
+
+				max_bw = max(max_bw, gpu_bw);
 			}
 
 			if (err == 0) {
@@ -153,6 +169,51 @@ static int igt_lmem_clear(void *arg)
 		intel_gt_pm_put(gt, wf);
 		if (err)
 			break;
+
+		if (measure && max_bw) {
+			struct pci_dev *pdev = to_pci_dev(gt->i915->drm.dev);
+			unsigned int expected = 0;
+
+			if (IS_PONTEVECCHIO(gt->i915)) {
+				u32 val;
+
+				if (snb_pcode_read_p(gt->uncore,
+						     XEHPSDV_PCODE_FREQUENCY_CONFIG,
+						     PCODE_MBOX_FC_SC_READ_FUSED_P0,
+						     PCODE_MBOX_DOMAIN_HBM,
+						     &val) == 0)
+					expected = _MBs(2 * 128 * val * GT_FREQUENCY_MULTIPLIER);
+			} else if (HAS_LMEM_MAX_BW(gt->i915)) {
+				u32 val;
+
+				if (snb_pcode_read_p(&i915->uncore, PCODE_MEMORY_CONFIG,
+						     MEMORY_CONFIG_SUBCOMMAND_READ_MAX_BANDWIDTH,
+						     0x0,
+						     &val) == 0)
+					expected = _MBs(val);
+			} else {
+				const struct pci_device_id *match;
+
+				match = pci_match_id(clear_bandwidth, pdev);
+				if (match)
+					expected = (uintptr_t)match->driver_data;
+			}
+
+			if (7 * max_bw > expected * 8) {
+				dev_warn(gt->i915->drm.dev,
+					 "[0x%04x.%d] Peak bw measured:%d MiB/s, beyond expected %d MiB/s\n",
+					 pdev->device, pdev->revision,
+					 max_bw, expected);
+			} else if (8 * max_bw < expected * 7) {
+				dev_err(gt->i915->drm.dev,
+					"[0x%04x.%d] Peak bw measured:%d MiB/s, expected at least 87.5%% of %d MiB/s [%d MiB/s]\n",
+					pdev->device, pdev->revision,
+					max_bw, expected,
+					7 * expected >> 3);
+				err = -ENXIO;
+				break;
+			}
+		}
 	}
 
 	if (CPU_LATENCY >= 0)
@@ -169,11 +230,30 @@ static int igt_lmem_clear(void *arg)
 	return err;
 }
 
+static int igt_lmem_clear(void *arg)
+{
+	return __igt_lmem_clear(arg, false);
+}
+
+static int igt_lmem_speed(void *arg)
+{
+	return __igt_lmem_clear(arg, true);
+}
+
 int i915_gem_lmem_live_selftests(struct drm_i915_private *i915)
 {
 	static const struct i915_subtest tests[] = {
 		SUBTEST(igt_lmem_touch),
 		SUBTEST(igt_lmem_clear),
+	};
+
+	return i915_live_subtests(tests, i915);
+}
+
+int i915_gem_lmem_wip_selftests(struct drm_i915_private *i915)
+{
+	static const struct i915_subtest tests[] = {
+		SUBTEST(igt_lmem_speed),
 	};
 
 	return i915_live_subtests(tests, i915);
