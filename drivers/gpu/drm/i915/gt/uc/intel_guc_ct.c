457bd800f471084d8925a7bc05f1f7c5e38a7f73
Chris Wilson <chris.p.wilson@linux.intel.com>
2023-03-18 08:01:52 +0000

    drm/i915/guc: Relieve interrupt pressure on the CT queues before sending
    In order to send a message, we need to make sure there is enough space
    for the ack/reply from the guc in our recv CT buffer. However, under
    sufficient interrupt pressure, draining the CT recv queue can be delayed
    and may not happen before we saturate the send queue leading to us
    declaring a CT communication stall and wedging the driver.
    
    To mitigate the impact of a delay in interrupt processing, check for an
    reply before performing the next send.
    
    Signed-off-by: Chris Wilson <chris.p.wilson@linux.intel.com>
    Reviewed-by: Mika Kuoppala <mika.kuoppala@linux.intel.com>

--- drivers/gpu/drm/i915/gt/uc/intel_guc_ct.c
+++ drivers/gpu/drm/i915/gt/uc/intel_guc_ct.c
@@ -13,6 +13,8 @@
 #include "intel_guc_ct.h"
 #include "gt/intel_gt.h"
 
+static void ct_try_receive_message(struct intel_guc_ct *ct);
+
 static inline struct intel_guc *ct_to_guc(struct intel_guc_ct *ct)
 {
 	return container_of(ct, struct intel_guc, ct);
@@ -778,6 +780,9 @@ int intel_guc_ct_send(struct intel_guc_ct *ct, const u32 *action, u32 len,
 	if (unlikely(ct->ctbs.send.broken))
 		return -EPIPE;
 
+	/* Drain the interrupt workqueue to prevent overfilling send/recv ct */
+	ct_try_receive_message(ct);
+
 	if (flags & INTEL_GUC_CT_SEND_NB)
 		return ct_send_nb(ct, action, len, flags);
 
@@ -912,7 +917,7 @@ static int ct_read(struct intel_guc_ct *ct, struct ct_incoming_msg **msg)
 	CT_DEBUG(ct, "received %*ph\n", 4 * len, (*msg)->msg);
 
 	/* update local copies */
-	ctb->head = head;
+	WRITE_ONCE(ctb->head, head);
 
 	/* now update descriptor */
 	WRITE_ONCE(desc->head, head);
@@ -1178,13 +1183,17 @@ static void ct_handle_msg(struct intel_guc_ct *ct, struct ct_incoming_msg *msg)
  */
 static int ct_receive(struct intel_guc_ct *ct)
 {
+	struct intel_guc_ct_buffer *ctb = &ct->ctbs.recv;
 	struct ct_incoming_msg *msg = NULL;
 	unsigned long flags;
 	int ret;
 
-	spin_lock_irqsave(&ct->ctbs.recv.lock, flags);
+	if (READ_ONCE(ctb->head) == READ_ONCE(ctb->desc->tail))
+		return 0;
+
+	spin_lock_irqsave(&ctb->lock, flags);
 	ret = ct_read(ct, &msg);
-	spin_unlock_irqrestore(&ct->ctbs.recv.lock, flags);
+	spin_unlock_irqrestore(&ctb->lock, flags);
 	if (ret < 0)
 		return ret;
 
@@ -1196,13 +1205,7 @@ static int ct_receive(struct intel_guc_ct *ct)
 
 static void ct_try_receive_message(struct intel_guc_ct *ct)
 {
-	int ret;
-
-	if (GEM_WARN_ON(!ct->enabled))
-		return;
-
-	ret = ct_receive(ct);
-	if (ret > 0)
+	if (ct_receive(ct) > 0)
 		tasklet_hi_schedule(&ct->receive_tasklet);
 }
 
@@ -1210,7 +1213,11 @@ static noinline void ct_receive_tasklet_func(struct tasklet_struct *t)
 {
 	struct intel_guc_ct *ct = from_tasklet(ct, t, receive_tasklet);
 
-	ct_try_receive_message(ct);
+	if (!ct->enabled)
+		return;
+
+	while (ct_receive(ct) > 0)
+		;
 }
 
 /*
