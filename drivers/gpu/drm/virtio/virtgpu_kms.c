f450298b5b12118f5abf3a3038dcf0584b12e44d
Hang Liu <hang1.liu@intel.com>
2023-07-01 12:30:18 +0800

    enable vblank for crtc
    virtio gpu backend inject virtual vblank interrupt
    for each virtio gpu crtc. The purpose for doing this
    is to avoid the potential image tearing caused by the
    concurrent accessing the same framebuffer by rendering
    engine in guest and display engine in host.
    
    Tracked-On: OAM-113127
    Signed-off-by: Hang Liu<hang1.liu@intel.com>

--- drivers/gpu/drm/virtio/virtgpu_kms.c
+++ drivers/gpu/drm/virtio/virtgpu_kms.c
@@ -118,7 +118,7 @@ int virtio_gpu_find_vqs(struct virtio_gpu_device *vgdev)
 	struct virtqueue **vqs;
 	int i, total_vqs, err;
 	const char **names;
-	int ret;
+	int ret = 0;
 
 	total_vqs = vgdev->num_vblankq + 2;
 	vqs = kcalloc(total_vqs, sizeof(*vqs), GFP_KERNEL);
@@ -164,6 +164,7 @@ int virtio_gpu_init(struct virtio_device *vdev, struct drm_device *dev)
 	struct virtio_gpu_device *vgdev;
 	u32 num_scanouts, num_capsets;
 	int ret = 0;
+	int i;
 
 	if (!virtio_has_feature(vdev, VIRTIO_F_VERSION_1))
 		return -ENODEV;
@@ -300,6 +301,12 @@ int virtio_gpu_init(struct virtio_device *vdev, struct drm_device *dev)
 
 	if (num_capsets)
 		virtio_gpu_get_capsets(vgdev, num_capsets);
+
+	virtio_gpu_vblankq_notify(vgdev);
+
+	for(i=0; i < vgdev->num_vblankq; i++)
+		virtqueue_disable_cb(vgdev->vblank[i].vblank.vq);
+
 	if (vgdev->has_edid)
 		virtio_gpu_cmd_get_edids(vgdev);
 	virtio_gpu_cmd_get_display_info(vgdev);
